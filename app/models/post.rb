a3a28a10d6e3ea24621969100ecf3c55c51e377c
houdamzari <houda.mzari1999@gmail.com>
2023-04-01 00:20:48 +0000

    Fix linter issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
   def update_post_counter
     author.update(posts_counter: author.posts_counter + 1)
   end
