5cbcd4b7c30b01e6eda62faef149d70beacef13a
houdamzari <houda.mzari1999@gmail.com>
2023-04-08 22:33:05 +0000

    fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   before_action :set_post, only: [:create]
 
-    def new
+  def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
   end
 
-    def create
+  def create
     @user = current_user
     @post = Post.find(params[:post_id])
     @comment = Comment.new(user: @user, post: @post, text: params[:comment][:text])
