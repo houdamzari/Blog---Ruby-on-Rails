6effeeab1e3ac7f048b41e4f0b150096c78e9bcc
houdamzari <houda.mzari1999@gmail.com>
2023-04-11 16:35:27 +0000

    Add user integration tests

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -1,37 +1,41 @@
 require 'rails_helper'
-require 'capybara/rspec'
 
-class UserIndexIntegrationTest < ActionDispatch::IntegrationTest
-  include Capybara::DSL
+RSpec.describe "UsersIndex", type: :system do
+  before do
+  @user1 = User.create!(name: 'User1', photo: 'https://my.alfred.edu/zoom/_images/foster-lake.jpg', bio: 'Bio for User1', postscounter: 2)
+  @post1 = @user1.posts.create!(title: 'Post 1 by User1', text: 'Post 1 by User1', comments_counter: 0, likes_counter: 0)
+  @post2 = @user1.posts.create!(title: 'Post 2 by User1', text: 'Post 2 by User1', comments_counter: 0, likes_counter: 0)
+  
+  @user2 = User.create!(name: 'User2', photo: 'https://my.alfred.edu/zoom/_images/foster-lake.jpg', bio: 'Bio for User2', postscounter: 1)
+  @post3 = @user2.posts.create!(title: 'Post 1 by User2', text: 'Post 1 by User2', comments_counter: 0, likes_counter: 0)
+end
 
-  def setup
-    @user1 = User.create!(name: 'User1', photo: 'photo1.jpg', bio: 'Bio for User1', postscounter: 0)
-    @user2 = User.create!(name: 'User2', photo: 'photo2.jpg', bio: 'Bio for User2', postscounter: 0)
 
-    @post1 = @user1.posts.create!(text: 'Post1 by User1')
-    @post2 = @user2.posts.create!(text: 'Post2 by User2')
+  after do
+    # Clean up created data
+    @user1.destroy if @user1.present?
+    @user2.destroy if @user2.present?
+    @post1.destroy if @post1.present?
+    @post2.destroy if @post2.present?
+    @post3.destroy if @post3.present?
   end
 
-  def teardown
-    @user1.destroy
-    @user2.destroy
-    @post1.destroy
-    @post2.destroy
-  end
+  it 'displays all users information' do
+    # Visit the users index page
+    visit users_path
 
-  test 'user index page' do
-    visit '/users'
+    # Check for the presence of user1 and user2 information
+    expect(page).to have_content 'User1'
+    expect(page).to have_content 'Number of posts: 2'
+    expect(page).to have_css("img[src*='https://my.alfred.edu/zoom/_images/foster-lake.jpg']")
+    expect(page).to have_link 'View Profile', href: user_path(@user1)
 
-    assert_text 'User1'
-    assert_text 'User2'
+    expect(page).to have_content 'User2'
+    expect(page).to have_content 'Number of posts: 1'
+    expect(page).to have_link 'View Profile', href: user_path(@user2)
 
-    assert_selector "img[src='photo1.jpg']"
-    assert_selector "img[src='photo2.jpg']"
-
-    assert_text 'Number of posts: 1'
-    assert_text 'Number of posts: 1'
-
-    click_link 'View Profile', href: user_path(@user1)
-    assert_current_path user_path(@user1)
+    # Check if clicking on a user redirects to the user's show page
+find("a[href='#{user_path(@user1.reload)}']").click
+expect(page).to have_current_path user_path(@user1.reload)
   end
 end
