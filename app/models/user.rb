22734a090b391cde5102357b48fac42b4580f019
houdamzari <houda.mzari1999@gmail.com>
2023-04-12 23:26:48 +0000

    Add user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,17 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :postscounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, on: :update
+
+  before_create :set_default_postscounter
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_postscounter
+    self.postscounter ||= 0
   end
 end
