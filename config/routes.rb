b7a9763f648bece9ae1b043777595623188968ea
houdamzari <houda.mzari1999@gmail.com>
2023-04-12 22:37:06 +0000

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'posts/:id', to: 'posts#show', as: 'post'
   resources :users, only: [:index, :show, :new] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
         resources :likes, only: [:new]
+        
   end
   end
 end
