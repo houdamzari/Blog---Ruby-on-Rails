216ba7bc978c95dc1d563a1a1015244f3229d82b
elarhadu <elarhadu@gmail.com>
2023-04-13 04:02:23 +0000

    Modify file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
